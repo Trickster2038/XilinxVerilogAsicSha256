b3542eec26410b79e51750488f2ec738c25db3cc
Sergey <fzastahov@gmail.com>
2022-02-19 22:50:02 +0300

    func t2

---
+++ Helloworld/HelloWorld/func_t2.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+`include "func_sigma0.v"
+`include "func_Ma.v"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:22:36 02/19/2022 
+// Design Name: 
+// Module Name:    func_t2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module func_t2(in_A, in_B, in_C, func);
+	
+	input wire[31:0] in_A;
+	input wire[31:0] in_B;
+	input wire[31:0] in_C;
+	output reg[31:0] func;
+	
+	wire[31:0] wire_sigma0;
+	wire[31:0] wire_Ma;
+	
+	func_sigma0 s0(.in_A(in_A), 
+		.func(wire_sigma0));
+	func_Ma ma(.in_A(in_A), .in_B(in_B),
+		.in_C(in_C), .func(wire_Ma));
+	
+	always @* begin
+		func = wire_sigma0 + wire_Ma;
+	end
+		
+endmodule
---
+++ Helloworld/HelloWorld/isim/func_t2_tb_isim_beh.exe.sim/work/m_00000000002080164034_1631405033.c
@@ -0,0 +1,383 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Professional/Desktop/XilinxVerilogAsicSha256/Helloworld/HelloWorld/func_Ma.v";
+
+
+
+static void Always_28_0(char *t0)
+{
+    char t7[8];
+    char t41[8];
+    char t72[8];
+    char t89[8];
+    char t120[8];
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    unsigned int t8;
+    unsigned int t9;
+    unsigned int t10;
+    char *t11;
+    char *t12;
+    unsigned int t13;
+    unsigned int t14;
+    unsigned int t15;
+    unsigned int t16;
+    unsigned int t17;
+    unsigned int t18;
+    unsigned int t19;
+    char *t20;
+    char *t21;
+    unsigned int t22;
+    unsigned int t23;
+    unsigned int t24;
+    unsigned int t25;
+    unsigned int t26;
+    unsigned int t27;
+    unsigned int t28;
+    unsigned int t29;
+    int t30;
+    int t31;
+    unsigned int t32;
+    unsigned int t33;
+    unsigned int t34;
+    unsigned int t35;
+    unsigned int t36;
+    unsigned int t37;
+    char *t38;
+    char *t39;
+    char *t40;
+    unsigned int t42;
+    unsigned int t43;
+    unsigned int t44;
+    char *t45;
+    char *t46;
+    unsigned int t47;
+    unsigned int t48;
+    unsigned int t49;
+    unsigned int t50;
+    unsigned int t51;
+    unsigned int t52;
+    unsigned int t53;
+    char *t54;
+    char *t55;
+    unsigned int t56;
+    unsigned int t57;
+    unsigned int t58;
+    unsigned int t59;
+    unsigned int t60;
+    unsigned int t61;
+    unsigned int t62;
+    unsigned int t63;
+    int t64;
+    int t65;
+    unsigned int t66;
+    unsigned int t67;
+    unsigned int t68;
+    unsigned int t69;
+    unsigned int t70;
+    unsigned int t71;
+    unsigned int t73;
+    unsigned int t74;
+    unsigned int t75;
+    char *t76;
+    char *t77;
+    char *t78;
+    unsigned int t79;
+    unsigned int t80;
+    unsigned int t81;
+    unsigned int t82;
+    unsigned int t83;
+    unsigned int t84;
+    unsigned int t85;
+    char *t86;
+    char *t87;
+    char *t88;
+    unsigned int t90;
+    unsigned int t91;
+    unsigned int t92;
+    char *t93;
+    char *t94;
+    unsigned int t95;
+    unsigned int t96;
+    unsigned int t97;
+    unsigned int t98;
+    unsigned int t99;
+    unsigned int t100;
+    unsigned int t101;
+    char *t102;
+    char *t103;
+    unsigned int t104;
+    unsigned int t105;
+    unsigned int t106;
+    unsigned int t107;
+    unsigned int t108;
+    unsigned int t109;
+    unsigned int t110;
+    unsigned int t111;
+    int t112;
+    int t113;
+    unsigned int t114;
+    unsigned int t115;
+    unsigned int t116;
+    unsigned int t117;
+    unsigned int t118;
+    unsigned int t119;
+    unsigned int t121;
+    unsigned int t122;
+    unsigned int t123;
+    char *t124;
+    char *t125;
+    char *t126;
+    unsigned int t127;
+    unsigned int t128;
+    unsigned int t129;
+    unsigned int t130;
+    unsigned int t131;
+    unsigned int t132;
+    unsigned int t133;
+    char *t134;
+
+LAB0:    t1 = (t0 + 2688U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(28, ng0);
+    t2 = (t0 + 3008);
+    *((int *)t2) = 1;
+    t3 = (t0 + 2720);
+    *((char **)t3) = t2;
+    *((char **)t1) = &&LAB4;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(28, ng0);
+
+LAB5:    xsi_set_current_line(29, ng0);
+    t4 = (t0 + 1048U);
+    t5 = *((char **)t4);
+    t4 = (t0 + 1208U);
+    t6 = *((char **)t4);
+    t8 = *((unsigned int *)t5);
+    t9 = *((unsigned int *)t6);
+    t10 = (t8 & t9);
+    *((unsigned int *)t7) = t10;
+    t4 = (t5 + 4);
+    t11 = (t6 + 4);
+    t12 = (t7 + 4);
+    t13 = *((unsigned int *)t4);
+    t14 = *((unsigned int *)t11);
+    t15 = (t13 | t14);
+    *((unsigned int *)t12) = t15;
+    t16 = *((unsigned int *)t12);
+    t17 = (t16 != 0);
+    if (t17 == 1)
+        goto LAB6;
+
+LAB7:
+LAB8:    t38 = (t0 + 1048U);
+    t39 = *((char **)t38);
+    t38 = (t0 + 1368U);
+    t40 = *((char **)t38);
+    t42 = *((unsigned int *)t39);
+    t43 = *((unsigned int *)t40);
+    t44 = (t42 & t43);
+    *((unsigned int *)t41) = t44;
+    t38 = (t39 + 4);
+    t45 = (t40 + 4);
+    t46 = (t41 + 4);
+    t47 = *((unsigned int *)t38);
+    t48 = *((unsigned int *)t45);
+    t49 = (t47 | t48);
+    *((unsigned int *)t46) = t49;
+    t50 = *((unsigned int *)t46);
+    t51 = (t50 != 0);
+    if (t51 == 1)
+        goto LAB9;
+
+LAB10:
+LAB11:    t73 = *((unsigned int *)t7);
+    t74 = *((unsigned int *)t41);
+    t75 = (t73 ^ t74);
+    *((unsigned int *)t72) = t75;
+    t76 = (t7 + 4);
+    t77 = (t41 + 4);
+    t78 = (t72 + 4);
+    t79 = *((unsigned int *)t76);
+    t80 = *((unsigned int *)t77);
+    t81 = (t79 | t80);
+    *((unsigned int *)t78) = t81;
+    t82 = *((unsigned int *)t78);
+    t83 = (t82 != 0);
+    if (t83 == 1)
+        goto LAB12;
+
+LAB13:
+LAB14:    t86 = (t0 + 1208U);
+    t87 = *((char **)t86);
+    t86 = (t0 + 1368U);
+    t88 = *((char **)t86);
+    t90 = *((unsigned int *)t87);
+    t91 = *((unsigned int *)t88);
+    t92 = (t90 & t91);
+    *((unsigned int *)t89) = t92;
+    t86 = (t87 + 4);
+    t93 = (t88 + 4);
+    t94 = (t89 + 4);
+    t95 = *((unsigned int *)t86);
+    t96 = *((unsigned int *)t93);
+    t97 = (t95 | t96);
+    *((unsigned int *)t94) = t97;
+    t98 = *((unsigned int *)t94);
+    t99 = (t98 != 0);
+    if (t99 == 1)
+        goto LAB15;
+
+LAB16:
+LAB17:    t121 = *((unsigned int *)t72);
+    t122 = *((unsigned int *)t89);
+    t123 = (t121 ^ t122);
+    *((unsigned int *)t120) = t123;
+    t124 = (t72 + 4);
+    t125 = (t89 + 4);
+    t126 = (t120 + 4);
+    t127 = *((unsigned int *)t124);
+    t128 = *((unsigned int *)t125);
+    t129 = (t127 | t128);
+    *((unsigned int *)t126) = t129;
+    t130 = *((unsigned int *)t126);
+    t131 = (t130 != 0);
+    if (t131 == 1)
+        goto LAB18;
+
+LAB19:
+LAB20:    t134 = (t0 + 1768);
+    xsi_vlogvar_wait_assign_value(t134, t120, 0, 0, 32, 0LL);
+    goto LAB2;
+
+LAB6:    t18 = *((unsigned int *)t7);
+    t19 = *((unsigned int *)t12);
+    *((unsigned int *)t7) = (t18 | t19);
+    t20 = (t5 + 4);
+    t21 = (t6 + 4);
+    t22 = *((unsigned int *)t5);
+    t23 = (~(t22));
+    t24 = *((unsigned int *)t20);
+    t25 = (~(t24));
+    t26 = *((unsigned int *)t6);
+    t27 = (~(t26));
+    t28 = *((unsigned int *)t21);
+    t29 = (~(t28));
+    t30 = (t23 & t25);
+    t31 = (t27 & t29);
+    t32 = (~(t30));
+    t33 = (~(t31));
+    t34 = *((unsigned int *)t12);
+    *((unsigned int *)t12) = (t34 & t32);
+    t35 = *((unsigned int *)t12);
+    *((unsigned int *)t12) = (t35 & t33);
+    t36 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t36 & t32);
+    t37 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t37 & t33);
+    goto LAB8;
+
+LAB9:    t52 = *((unsigned int *)t41);
+    t53 = *((unsigned int *)t46);
+    *((unsigned int *)t41) = (t52 | t53);
+    t54 = (t39 + 4);
+    t55 = (t40 + 4);
+    t56 = *((unsigned int *)t39);
+    t57 = (~(t56));
+    t58 = *((unsigned int *)t54);
+    t59 = (~(t58));
+    t60 = *((unsigned int *)t40);
+    t61 = (~(t60));
+    t62 = *((unsigned int *)t55);
+    t63 = (~(t62));
+    t64 = (t57 & t59);
+    t65 = (t61 & t63);
+    t66 = (~(t64));
+    t67 = (~(t65));
+    t68 = *((unsigned int *)t46);
+    *((unsigned int *)t46) = (t68 & t66);
+    t69 = *((unsigned int *)t46);
+    *((unsigned int *)t46) = (t69 & t67);
+    t70 = *((unsigned int *)t41);
+    *((unsigned int *)t41) = (t70 & t66);
+    t71 = *((unsigned int *)t41);
+    *((unsigned int *)t41) = (t71 & t67);
+    goto LAB11;
+
+LAB12:    t84 = *((unsigned int *)t72);
+    t85 = *((unsigned int *)t78);
+    *((unsigned int *)t72) = (t84 | t85);
+    goto LAB14;
+
+LAB15:    t100 = *((unsigned int *)t89);
+    t101 = *((unsigned int *)t94);
+    *((unsigned int *)t89) = (t100 | t101);
+    t102 = (t87 + 4);
+    t103 = (t88 + 4);
+    t104 = *((unsigned int *)t87);
+    t105 = (~(t104));
+    t106 = *((unsigned int *)t102);
+    t107 = (~(t106));
+    t108 = *((unsigned int *)t88);
+    t109 = (~(t108));
+    t110 = *((unsigned int *)t103);
+    t111 = (~(t110));
+    t112 = (t105 & t107);
+    t113 = (t109 & t111);
+    t114 = (~(t112));
+    t115 = (~(t113));
+    t116 = *((unsigned int *)t94);
+    *((unsigned int *)t94) = (t116 & t114);
+    t117 = *((unsigned int *)t94);
+    *((unsigned int *)t94) = (t117 & t115);
+    t118 = *((unsigned int *)t89);
+    *((unsigned int *)t89) = (t118 & t114);
+    t119 = *((unsigned int *)t89);
+    *((unsigned int *)t89) = (t119 & t115);
+    goto LAB17;
+
+LAB18:    t132 = *((unsigned int *)t120);
+    t133 = *((unsigned int *)t126);
+    *((unsigned int *)t120) = (t132 | t133);
+    goto LAB20;
+
+}
+
+
+extern void work_m_00000000002080164034_1631405033_init()
+{
+	static char *pe[] = {(void *)Always_28_0};
+	xsi_register_didat("work_m_00000000002080164034_1631405033", "isim/func_t2_tb_isim_beh.exe.sim/work/m_00000000002080164034_1631405033.didat");
+	xsi_register_executes(pe);
+}
